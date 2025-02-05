56b5be8b0f14903e6fa32ce2fc496182d5b50962
marie270539710 <270539710@yoobeestudent.ac.nz>
2025-02-05 15:15:50 +1300

    matplotlib samples

--- week9/lineplot.py
+++ week9/lineplot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 x = [1, 2, 3, 4, 5]
 y = [10, 20, 25, 30, 40]
-plt.plot(x, y, marker='o')
+plt.plot(x, y, marker='*', color='red')
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.title('Basic Line Plot')
