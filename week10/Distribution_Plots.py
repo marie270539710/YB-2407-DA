5554ab345f96fdc2245834b72376cfaf2e4ba6e0
marie270539710 <270539710@yoobeestudent.ac.nz>
2025-02-12 16:07:31 +1300

    activity updated

--- week10/Distribution_Plots.py
+++ week10/Distribution_Plots.py
@@ -11,15 +11,23 @@ df = pd.read_csv("week10/Sample_Data_for_Activity.csv")  # Replace with your CSV
 #sns.histplot(df["Normal_Distribution"])  
 #sns.displot(df['Normal_Distribution'], kde=True)
 sns.displot(df['Normal_Distribution'], kde=True, bins=200)
+plt.savefig("week10/displot.png")
 
 #sns.jointplot(x='Normal_Distribution', y='Exponential_Distribution', data=df, kind='scatter')
 sns.jointplot(x='Normal_Distribution', y='Exponential_Distribution', data=df, kind='reg')
 #sns.jointplot(x='Normal_Distribution', y='Exponential_Distribution', data=df, kind='hex')
+plt.savefig("week10/jointplot.png")
 
 #sns.jointplot(x='Normal_Distribution', y='Exponential_Distribution', data=df, hue='Poisson_Distribution')
 #sns.jointplot(x='Normal_Distribution', y='Exponential_Distribution', data=df, hue='Poisson_Distribution', kind='kde')
 
 sns.pairplot(df)
+plt.savefig("week10/pairplot.png")
+
+plt.figure(figsize=(10, 6))  # Adjust figure size
+corr_matrix = df.corr()
+sns.heatmap(corr_matrix, annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5)
+plt.savefig("week10/heatmap.png")
 
 # Show the plot
 plt.show()
